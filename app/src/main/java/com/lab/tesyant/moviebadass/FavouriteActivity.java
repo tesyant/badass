daf6a09cd02c23cdf02d7a35f9a1d3bd57b3776a
tesya <tesyant@gmail.com>
2017-10-23 00:59:12 +0700

    fixing favActivity

--- app/src/main/java/com/lab/tesyant/moviebadass/FavouriteActivity.java
+++ app/src/main/java/com/lab/tesyant/moviebadass/FavouriteActivity.java
@@ -1,8 +1,6 @@
 package com.lab.tesyant.moviebadass;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,7 +11,6 @@ import com.lab.tesyant.moviebadass.adapter.FavAdapter;
 import com.lab.tesyant.moviebadass.db.FavouriteHelper;
 import com.lab.tesyant.moviebadass.model.Results;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class FavouriteActivity extends AppCompatActivity implements View.OnClickListener {
@@ -36,8 +33,6 @@ public class FavouriteActivity extends AppCompatActivity implements View.OnClick
         rvFav.setLayoutManager(new LinearLayoutManager(this));
         rvFav.setHasFixedSize(true);
 
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
-
         favouriteHelper = new FavouriteHelper(this);
         favouriteHelper.open();
 
@@ -46,8 +41,6 @@ public class FavouriteActivity extends AppCompatActivity implements View.OnClick
         favAdapter = new FavAdapter(this);
         favAdapter.setListFav(list);
         rvFav.setAdapter(favAdapter);
-
-        new LoadFavAsync().execute();
     }
 
     @Override
@@ -59,37 +52,4 @@ public class FavouriteActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View view) {
     }
 
-    public class LoadFavAsync extends AsyncTask<Void, Void, ArrayList<Results>> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressBar.setVisibility(View.VISIBLE);
-
-            if (list.size() > 0) {
-                list.clear();
-            }
-        }
-
-        @Override
-        protected ArrayList<Results> doInBackground(Void... voids) {
-            return favouriteHelper.query();
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Results> resultses) {
-            super.onPostExecute(resultses);
-            progressBar.setVisibility(View.GONE);
-            list.addAll(resultses);
-            favAdapter.setListFav(list);
-            favAdapter.notifyDataSetChanged();
-
-            if (list.size() == 0) {
-                showSnackbarMessage("Tidak ada data saat ini");
-            }
-        }
-    }
-
-    private void showSnackbarMessage(String message) {
-        Snackbar.make(rvFav, message, Snackbar.LENGTH_SHORT).show();
-    }
 }
